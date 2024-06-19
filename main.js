9abe7408fa7918002fd2713887cf959927731c25
liliia_vintiuk <rudapanda2016@gmail.com>
2024-06-19 19:22:01 +0300

    initial

---
+++ main.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+    navLinks.forEach(link => {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
